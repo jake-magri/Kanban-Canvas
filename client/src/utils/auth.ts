66835ad979c32f0307950e8af3351422c347f279
Bread <jmagri1@uncc.edu>
2024-11-05 20:06:48 -0500

    added getProfile, loggedIn, getToken, login, and logout methods

--- client/src/utils/auth.ts
+++ client/src/utils/auth.ts
@@ -3,10 +3,14 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode(this.getToken());
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    // Check if the user is logged in by retrieving the token from localStorage
+    const token = this.getToken();
+    return token;
   }
   
   isTokenExpired(token: string) {
@@ -15,16 +19,23 @@ class AuthService {
 
   getToken(): string {
     // TODO: return the token
+    // Retrieve the JWT token from localStorage
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser;
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem('id_token');
+    window.location.assign('/');
   }
 }
 
