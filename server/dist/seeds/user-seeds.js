6fe27f138c2213c206c0c918c2229989cec8b6a5
Bread <jmagri1@uncc.edu>
2024-11-18 23:10:30 -0500

    render debugging

--- server/dist/seeds/user-seeds.js
+++
@@ -1,8 +0,0 @@
-import { User } from '../models/user.js';
-export const seedUsers = async () => {
-    await User.bulkCreate([
-        { username: 'JollyGuru', email: 'test@gmail.com', password: 'password' },
-        { username: 'SunnyScribe', email: 'test2@gmail.com', password: 'password' },
-        { username: 'RadiantComet', email: 'test3@gmail.com', password: 'password' },
-    ], { individualHooks: true });
-};
