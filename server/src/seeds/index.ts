ccedae1aacef30af97021e8ebe1075b62bca292c
Bread <jmagri1@uncc.edu>
2024-11-18 23:31:17 -0500

    render debugging

--- server/src/seeds/index.ts
+++ server/src/seeds/index.ts
@@ -1,11 +1,11 @@
 import { seedUsers } from './user-seeds.js';
 import { seedTickets } from './ticket-seeds.js';
-import { sequelize } from '../models/index.js';
+// import { sequelize } from '../models/index.js';
 
 const seedAll = async (): Promise<void> => {
   try {
-    await sequelize.sync({ force: true });
-    console.log('\n----- DATABASE SYNCED -----\n');
+    // await sequelize.sync({ force: true });
+    // console.log('\n----- DATABASE SYNCED -----\n');
     
     await seedUsers();
     console.log('\n----- USERS SEEDED -----\n');
